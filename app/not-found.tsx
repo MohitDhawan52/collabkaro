335c6f171d0f0374cf0d86a0dbf8c97d37dd2660
MohitDhawan52 <dhawanmohi@gmail.com>
2026-06-15 01:25:47 +0530

    Fix static generation hang

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export const dynamic = 'force-dynamic'
+
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: 'center', padding: '100px 24px' }}>
+      <h1>404 - Page Not Found</h1>
+      <a href="/">Go Home</a>
+    </div>
+  )
+}
